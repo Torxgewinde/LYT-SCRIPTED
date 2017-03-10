9afdb76b5b425ea2b0ab51199b380c9480a9ec98
Torxgewinde <Torxgewinde@users.noreply.github.com>
2017-03-10 16:59:27 +0100

    Added a gamma function to correct the colors

--- a40_LED.ino
+++ a40_LED.ino
@@ -27,6 +27,39 @@
 #define POWER_ENABLE_LED 15
 
 /******************************************************************************
+Description.: The visual impression of the LEDs is not linear like the PWM,
+              thus to make the range match the visual impression better
+              a gamma correction is usual. Here, just one color channel
+              is adjusted, which fixes the most important issues. Dependencies
+              of one color with the others is not improved this way, so color
+              shifts in mixed colors are still possible.
+              Credit for the LUT values goes to
+              http://rgb-123.com/ws2812-color-output/
+Input Value.: brightness level for one color (0 .. 255)
+Return Value: gamma corrected value (0 .. 255)
+******************************************************************************/
+uint8_t gamma_correction(uint8_t value) {
+  uint8_t gammaE[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2,
+    2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5,
+    6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 11, 11,
+    11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18,
+    19, 19, 20, 21, 21, 22, 22, 23, 23, 24, 25, 25, 26, 27, 27, 28,
+    29, 29, 30, 31, 31, 32, 33, 34, 34, 35, 36, 37, 37, 38, 39, 40,
+    40, 41, 42, 43, 44, 45, 46, 46, 47, 48, 49, 50, 51, 52, 53, 54,
+    55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
+    71, 72, 73, 74, 76, 77, 78, 79, 80, 81, 83, 84, 85, 86, 88, 89,
+    90, 91, 93, 94, 95, 96, 98, 99,100,102,103,104,106,107,109,110,
+    111,113,114,116,117,119,120,121,123,124,126,128,129,131,132,134,
+    135,137,138,140,142,143,145,146,148,150,151,153,155,157,158,160,
+    162,163,165,167,169,170,172,174,176,178,179,181,183,185,187,189,
+    191,193,194,196,198,200,202,204,206,208,210,212,214,216,218,220,
+    222,224,227,229,231,233,235,237,239,241,244,246,248,250,252,255};
+  
+  return gammaE[value];
+}
+
+/******************************************************************************
 Description.: set the LED, if RGB are set to the same value the white LEDs are
               used. Operating White and RGB LEDs is not OK according to
               the manufacturer (operating all at once would damage the bulb).
@@ -38,11 +71,11 @@ void setLeds(uint8_t red, uint8_t green, uint8_t blue) {
     analogWrite(RED_PIN, 0);
     analogWrite(GREEN_PIN, 0);
     analogWrite(BLUE_PIN, 0);
-    analogWrite(WHITE_PIN, red);
+    analogWrite(WHITE_PIN, gamma_correction(red));
   } else {
-    analogWrite(RED_PIN, red);
-    analogWrite(GREEN_PIN, green);
-    analogWrite(BLUE_PIN, blue);
+    analogWrite(RED_PIN, gamma_correction(red));
+    analogWrite(GREEN_PIN, gamma_correction(green));
+    analogWrite(BLUE_PIN, gamma_correction(blue));
     analogWrite(WHITE_PIN, 0);
   }
   
